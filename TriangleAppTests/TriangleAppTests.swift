bb6fa00612810acabc325fc485a4083759135fd2
Budi Darmawan <budymitman_77@yahoo.co.id>
2021-12-02 13:50:55 +0700

    add test

--- TriangleAppTests/TriangleAppTests.swift
+++ TriangleAppTests/TriangleAppTests.swift
@@ -36,6 +36,14 @@ class TriangleAppTests: XCTestCase {
     }
   }
   
+  func testDetectRandomTriangle() {
+    XCTAssertEqual(try detectTriangle(3, 5, 7), "Segitiga Sembarang")
+  }
+  
+  func testDetectPythagorasTriangle() {
+    XCTAssertEqual(try detectTriangle(6, 8, 10), "Segitiga Siku-Siku")
+  }
+  
   func detectTriangle(
     _ sideA: Int,
     _ sideB: Int,
@@ -55,6 +63,8 @@ class TriangleAppTests: XCTestCase {
       return "Segitiga Sama Sisi"
     } else if sides[0] == sides[1] || sides[1] == sides[2] {
       return "Segitiga Sama Kaki"
+    } else if Double((sides[0] * sides[0] + sides[1] * sides[1])).squareRoot() == Double(sides[2]) {
+      return "Segitiga Siku-Siku"
     } else {
       return "Segitiga Sembarang"
     }
